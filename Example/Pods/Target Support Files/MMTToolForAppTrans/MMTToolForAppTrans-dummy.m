90e1998037d5eb5bcf12fd4066d78a828e33234a
Donghn <Donghn@maxeye.com>
2026-04-22 14:42:48 +0800

    chore(repo): remove tracked Example Pods from repository

--- Example/Pods/Target Support Files/MMTToolForAppTrans/MMTToolForAppTrans-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MMTToolForAppTrans : NSObject
-@end
-@implementation PodsDummy_MMTToolForAppTrans
-@end
--- Example/Pods/Target Support Files/MMTToolForXCGLog/MMTToolForXCGLog-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MMTToolForXCGLog : NSObject
-@end
-@implementation PodsDummy_MMTToolForXCGLog
-@end
